c5afdccefab508b541d58385162a75e287a5f406
Damian Rawski <damian.rawski@schibsted.pl>
2017-12-06 08:31:07 +0100

    Added support of different severity levels in the XML report

--- src/reporter.js
+++ src/reporter.js
@@ -4,6 +4,17 @@ const path = require('path');
 const { format } = require('util');
 const checkstyle = require('checkstyle-formatter');
 
+function cvssScoreToSeverity(score) {
+    if (score < 4.0) {
+        return 'info';
+    } else if (score >= 4.0 && score < 7.0) {
+        return 'warning';
+    } else if (score >= 7.0) {
+        return 'error';
+    }
+    return 'ignore';
+}
+
 module.exports.error = function (error, args) {
     console.error(error.toString());
 };
@@ -15,12 +26,12 @@ module.exports.success = function (result, args) {
 module.exports.check = {
     success(result, args) {
         const messages = result.data.map(function (item) {
-            const { module, title, vulnerable_versions, patched_versions, version, advisory } = item;
+            const { module, title, vulnerable_versions, patched_versions, version, advisory, cvss_score } = item;
 
             return {
                 line: 0,
                 column: 0,
-                severity: 'error',
+                severity: cvssScoreToSeverity(cvss_score),
                 message: format(
                     'Module %s has a known vulnerability: "%s" (vulnerable: %s, patched: %s, yours: %s), see %s',
                     module, title, vulnerable_versions, patched_versions, version, advisory
